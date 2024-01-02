3497db18122d8c18dff4c23fe3d1633f9e7691b9
Antoni Przybylik <antoni.przybylik@wp.pl>
2024-01-02 20:09:42 +0000

    Added send_one_leg_frame to header.

--- spi.h
+++ spi.h
@@ -4,4 +4,7 @@
 int hhc_register(void);
 void hhc_deregister(void);
 
+int send_one_servo_frame(struct spi_device *spi, uint8_t servo_id,
+			 uint8_t servo_op_type, angle_t angle);
+
 #endif /* SPI_H_ */
